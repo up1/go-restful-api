2d3781c574a453beae2607c1d25d26c3a8e24bf3
somkiat <somkiat.p@gmail.com>
2018-07-10 01:37:26 +0700

    Working with docker

--- search_api/step_02/run.sh
+++ search_api/step_02/run.sh
@@ -1,6 +1,8 @@
 CURDIR=`pwd`
 OLDGOPATH=$GOPATH
 export GOPATH=$CURDIR
+# go get gopkg.in/mgo.v2
+# go get gopkg.in/tomb.v2
 gofmt -w src/
 go test -v -cover -coverprofile=coverage.out  ./...
 go install main
