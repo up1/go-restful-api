8ff6b76fd11b07f50a42e5f431e40f93f410ff85
somkiat <somkiat.p@gmail.com>
2018-07-09 22:18:59 +0700

    Step 2 working with json response

---
+++ simple-api/step_02.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+  "log"
+  "encoding/json"
+)
+
+type helloWorldResponse struct {
+  Header headerResponse  `json:"header"`
+  Body bodyResponse `json:"body"`
+}
+
+type headerResponse struct {
+  Code int `json:"code"`
+  Description string `json:"description"`
+}
+
+type bodyResponse struct {
+  Message string `json:"message"`
+}
+
+func main() {
+  port := 8080
+  http.HandleFunc("/hello", helloHandler)
+  log.Printf("Server starting on port %v\n", port)
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+  log.Printf("Called helloHandler\n")
+  response := helloWorldResponse {
+    Header: headerResponse{
+      Code: 200,
+      Description: "Success",
+    },
+    Body: bodyResponse { Message: "Hello World" },
+  }
+  data, err := json.Marshal(response)
+	if err != nil {
+		panic("Ooops")
+	}
+  fmt.Fprint(w, string(data))
+}
