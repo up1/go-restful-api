dd84b72c0265549196c949de5e40999fe907b164
somkiat <somkiat.p@gmail.com>
2018-07-10 01:20:50 +0700

    Main program

--- search_api/step_02/src/main/main.go
+++ search_api/step_02/src/main/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"api"
+	"repository"
+)
+
+func main() {
+	serverURI := "localhost"
+	if os.Getenv("MONGODB_SERVER") != "" {
+		serverURI = os.Getenv("MONGODB_SERVER")
+	}
+
+	store, err := repository.NewMongoStore(serverURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := api.Search{DataStore: store}
+	err = http.ListenAndServe(":8080", &handler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
